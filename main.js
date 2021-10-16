0f14fcbca1012ee139b6585009d4573935e947a3
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-16 16:14:11 +0200

    Designed the level, still stable

--- main.js
+++ main.js
@@ -13,12 +13,46 @@ add([
 ]);
 
 loadSprite("hero", "media/mario.png")
-loadSprite("enemy", "media/enemy.png")
+loadSprite("enemy", "media/ennemy.png")
 loadSprite("item-box", "media/item-box.png")
 loadSprite("wall", "media/wall.png")
 
 
-add([
+const player = add([
     sprite("hero"),
+    scale(2),
     pos(200, 200),
 ])
+
+addLevel([
+    "                           ",
+    "                           ",
+    "                           ",
+    "                      =    ",
+    "         ====         =    ",
+    "                      =    ",
+    "                      =    ",
+    "               =           ",
+    "===========================",
+], {
+    // define the size of each block
+    width: 40,
+    height: 40,
+    // define what each symbol means, by a function returning a comp list (what you'll pass to add())
+    "=": () => [
+        sprite("wall"),
+        area(),
+        solid(),
+        scale(2)
+    ],
+    // "$": () => [
+    //     sprite("coin"),
+    //     area(),
+    //     pos(0, -9),
+    // ],
+    // "^": () => [
+    //     sprite("spike"),
+    //     area(),
+    //     "danger",
+    // ],
+});
