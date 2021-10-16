de9d08fe0db4c0b7cf3326b3b4b2fbcc405487d8
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-16 14:42:32 +0200

    Setup of the game

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-kaboom({"fullscreen":false, "width":1280, "height":1024}
-
-)
+kaboom({
+    width:1280, 
+    height:1024,
+    debug:true,
+    stretch: true,
+    letterbox: true,
+    background: [0, 0, 0]
+})
 
 add([
     text("Shitty Mario"),
